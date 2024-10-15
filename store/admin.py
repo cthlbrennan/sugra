ae3f58329c1ecd1e50ee8ee0b845baaff928b9f2
Cathal Brennan <cthlbrennan@gmail.com>
2024-10-15 09:07:43 +0000

    Register models on admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User, Game, Wishlist, Order, OrderLine, Review, Screenshot, Message
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Game)
+admin.site.register(Wishlist)
+admin.site.register(Order)
+admin.site.register(OrderLine)
+admin.site.register(Review)
+admin.site.register(Screenshot)
+admin.site.register(Message)
